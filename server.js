0a612a4f3d31d62546c33c59f559c7d0fdcf5254
Igor Volkov Denisovich <cabakapro@gmail.com>
2026-04-08 21:37:18 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -139,6 +139,10 @@ app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.get("/api/products", (req, res) => {
   try {
     const products = readProductsFromExcel();
